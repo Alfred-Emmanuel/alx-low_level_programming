6f3a2c9d8e40e9763090358cc26858088848bfe1
Alfred Emmanuel <ealfred828@gmail.com>
2022-04-10 14:31:49 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #1nclude <studio.h>
 /**
-* main - entry point
-*
-* Return: 0 after printing the function
-*/
+ *main - entry point
+ *
+ *Return: 0 after printing the function
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
