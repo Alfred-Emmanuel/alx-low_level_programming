98f531a2e43a98716c08bb8eb7a2f2a32fe54fdd
Alfred Emmanuel <ealfred828@gmail.com>
2022-04-12 06:44:41 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
- * main - entry point
+ * main - entry block
  * @void: no argument
  * Return: 0
  **/
-int main()
+int main(void)
 {
 	char ermias[] = "_putchar";
+
 	int c;
+
 	for (c = 0; c < 8; c++)
 	{
 		_putchar(ermias[c]);
