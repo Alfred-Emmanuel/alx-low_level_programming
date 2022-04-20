3f760d2599618c6987084391814acd519db40ab9
Alfred Emmanuel <ealfred828@gmail.com>
2022-04-20 15:51:08 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * writng a function that takes a pointer to an integar
+ *
+ * and update the value it points to to 98
+ * always return 0
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+return (0);
