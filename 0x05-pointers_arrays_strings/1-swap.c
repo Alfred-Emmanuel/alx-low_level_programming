8a450f8be86c0e829388ba19fc828e2c988d7b38
Alfred Emmanuel <ealfred828@gmail.com>
2022-04-20 17:11:16 -0700

    task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *swap_int  swap value of two intergers
+ *swap_int - swap value of two intergers
  *
  * @a: value of first integer
  * @b: value of second integer
@@ -9,7 +9,7 @@
 void swap_int(int *a, int *b)
 {
 	int c;
-	
+
 	c = 0;
 	c = *a;
 	*a = *b;
